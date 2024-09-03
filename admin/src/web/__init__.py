d42f3674ec0028a7d77abbdf5f4903d355ba1017
GIULIANA CESTORAME <giulianaacestorame@gmail.com>
2024-09-03 17:16:05 -0300

    agrego plantillas

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
-def create_app(env="development", static_folder=""):
+def create_app(env="development", static_folder="../../static"):
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=static_folder)
 
     @app.route("/")
     def home(): 
-        return "¡Hola mundo!"
+        return render_template("home.html")
     
     return app
