a6422ff4bc22b79946f9131ecd8658de066ba310
Ezequiel Reale <ezequielreale33@gmail.com>
2024-09-29 16:02:51 -0300

    Agrego chequeos

--- admin/src/core/people/__init__.py
+++ admin/src/core/people/__init__.py
@@ -6,9 +6,12 @@ def list_members()->list:
     """Devuelve todos los miembros de la BD"""
     return Member.query.all()
 
-def get_member_by_id(member_id:int)->Member:
-    """Devuelve un miembro por ID"""
-    return Member.query.filter_by(id=member_id).first()
+def get_member_by_field(field: str, value, exclude_id=None) -> Member:
+    """Devuelve un miembro por un campo específico y su valor"""
+    query = Member.query.filter_by(**{field: value})
+    if exclude_id is not None:
+        query = query.filter(Member.id != exclude_id)
+    return query.first()
 
 def member_new(**kwargs)->Member:
     """Crea un miembro, lo guarda en la BD y lo devuelve"""
@@ -19,7 +22,7 @@ def member_new(**kwargs)->Member:
 
 def member_update(member_id:int, **kwargs)->Member:
     """Actualiza un miembro por ID y lo devuelve"""
-    member = get_member_by_id(member_id)
+    member = get_member_by_field('id', member_id)
     for attr, value in kwargs.items():
         setattr(member, attr, value)
     db.session.commit()
@@ -27,7 +30,7 @@ def member_update(member_id:int, **kwargs)->Member:
 
 def member_delete(member_id:int)->Member:
     """Elimina un miembro por ID"""
-    member = get_member_by_id(member_id)
+    member = get_member_by_field('id', member_id)
     db.session.delete(member)
     db.session.commit()
     return member
--- admin/src/web/controllers/team.py
+++ admin/src/web/controllers/team.py
@@ -1,28 +1,21 @@
-from flask import render_template, Blueprint, request, redirect, url_for
+from flask import Blueprint, flash, redirect, render_template, Request, request, url_for
 
-from src.core import people, adressing, professions
-
+from src.core import adressing, people, professions
+from web.forms.member_form import MemberForm
 
 bp = Blueprint("team", __name__, url_prefix ="/team") 
 
 @bp.get("/")
-def index():
+def index()->str:
     """Listado de miembros del equipo"""
     members = people.list_members()
     return render_template('team/index.html', members=members)
 
 @bp.get("/<int:id>")
-def show(id: int):
+def show(id: int)->str:
     """Detalle de un miembro del equipo"""
-    member = people.get_member_by_id(id)
+    member = people.get_member_by_field('id', id)
     return render_template('team/show.html', member=member)
-
-def _check_data(form_data: dict):
-    """Valida los datos del formulario"""
-    form_data['active'] = form_data.get('active', 'false').lower() == 'true'
-    if form_data.get('end_date') == '':
-        form_data['end_date'] = None
-    return form_data
 
 def _get_data_from_db():
     """Obtiene los datos de la base de datos"""
@@ -32,40 +25,62 @@ def _get_data_from_db():
     localities = adressing.list_localities()
     return profession_list, jobs, provinces, localities
 
-@bp.route("/nuevo" , methods=['GET', 'POST'])
-def create():
-    """Formulario para crear un nuevo miembro del equipo"""
-    profession_list, jobs, provinces, localities = _get_data_from_db()
-    
-    # Si se envia el formulario
-    if request.method == "POST":
-        if 'search_localities' in request.form:
-            province_id = request.form['province_id']
-            localities = adressing.get_localities_by_province(province_id)
-            return render_template('team/create.html', professions=profession_list, jobs=jobs, provinces=provinces, localities=localities)
-        else:
-            form_data = _check_data(request.form.to_dict())
-            member = people.member_new(**form_data)
-            return redirect(url_for('team.show', id=member.id))
-    
-    return render_template('team/create.html', professions=profession_list, jobs=jobs, provinces=provinces, localities=localities)
+def _get_validator(profession_list:list, jobs:list, localities:list, member_id=None)->MemberForm:
+    """Configura los campos del formulario"""
+    form = MemberForm(member_id=member_id)
+    form.profession_id.choices = [(profession.id, profession.name) for profession in profession_list]
+    form.job_id.choices = [(job.id, job.name) for job in jobs]
+    form.locality_id.choices = [(locality.id, locality.name) for locality in localities]
+    return form
 
-@bp.route("/<int:id>/editar", methods=['GET', 'POST'])
-def update(id: int):
-    """Formulario para editar un miembro del equipo"""
-    member = people.get_member_by_id(id)
+def _get_form(req:Request, profession_list:list, jobs:list, provinces:list, localities: list, member_id=None)->tuple:
+    """Valida los datos del formulario"""
+    form = _get_validator(profession_list, jobs, localities, member_id)
+    form.process(req.form)
+    if not form.validate():
+        error_messages = [f"{field}: {', '.join(errors)}" for field, errors in form.errors.items()]
+        flash(f"Error en el formulario -> {'; '.join(error_messages)}", "danger")
+        return form, False
+    
+    return form, True
+
+@bp.route("/new" , methods=['GET', 'POST'])
+def create()->str:
+    """Formulario para crear un nuevo miembro del equipo"""
+    member = {}
     profession_list, jobs, provinces, localities = _get_data_from_db()
     
     # Si se envia el formulario
     if request.method == "POST":
-        form_data = _check_data(request.form.to_dict())
-        member = people.member_update(id, **form_data)
-        return redirect(url_for('team.show', id=member.id))
+            form, valid = _get_form(request, profession_list, jobs, provinces, localities)
+            if valid:
+                member = people.member_new(**form.data)
+                return redirect(url_for('team.show', id=member.id))
+            else:
+                member = form.data
+
+    return render_template('team/create.html', member=member, professions=profession_list, jobs=jobs, provinces=provinces, localities=localities)
+
+@bp.route("/<int:id>/edit", methods=['GET', 'POST'])
+def update(id: int)->str:
+    """Formulario para editar un miembro del equipo"""
+    member = people.get_member_by_field('id', id)
+    profession_list, jobs, provinces, localities = _get_data_from_db()
+    
+    # Si se envia el formulario
+    if request.method == "POST":
+        form, valid = _get_form(request, profession_list, jobs, provinces, localities, member_id=member.id)
+        if valid:
+            member = people.member_update(id, **form.data)
+            return redirect(url_for('team.show', id=member.id))
+        else:
+            member = form.data
     
     return render_template('team/update.html', member=member, professions=profession_list, jobs=jobs, provinces=provinces, localities=localities)
 
-@bp.post("/<int:id>/eliminar")
-def destroy(id: int):
+@bp.post("/<int:id>/delete")
+def destroy(id: int)->str:
     """Eliminar un miembro del equipo"""
     member = people.member_delete(id)
+    flash(f"El miembro {member.name} {member.last_name} ha sido eliminado", "success")
     return redirect(url_for('team.index'))
