7b0609b110a25883d79f0455312c3d3d051415eb
Ezequiel Reale <ezequielreale33@gmail.com>
2024-10-16 19:33:02 -0300

    Arreglo los create table

--- DB_setup/job_proposals.sql
+++ DB_setup/job_proposals.sql
@@ -1,15 +1,43 @@
-CREATE TABLE job_proposals (
-    id SERIAL PRIMARY KEY,
+DROP TABLE IF EXISTS public.job_proposals;
+
+CREATE TABLE IF NOT EXISTS public.job_proposals
+(
+    id SERIAL NOT NULL,  -- Cambiado a SERIAL para auto-incrementar
     institutional_work_proposal institutional_work_proposals NOT NULL,
     condition conditions NOT NULL,
     headquarters headquarters NOT NULL,
-    days VARCHAR[] NOT NULL,
-    rider_id INTEGER NOT NULL REFERENCES riders(id),
-    professor_id INTEGER NOT NULL REFERENCES members(id),
-    member_horse_rider_id INTEGER NOT NULL REFERENCES members(id),
-    horse_id INTEGER NOT NULL REFERENCES horses(id),
-    assistant_id INTEGER NOT NULL REFERENCES members(id)
+    days days_of_week[] NOT NULL,
+    rider_id integer NOT NULL,
+    professor_id integer NOT NULL,
+    member_horse_rider_id integer NOT NULL,
+    horse_id integer NOT NULL,
+    assistant_id integer NOT NULL,
+    CONSTRAINT job_proposals_pkey PRIMARY KEY (id),
+    CONSTRAINT job_proposals_assistant_id_fkey FOREIGN KEY (assistant_id)
+        REFERENCES public.members (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT job_proposals_horse_id_fkey FOREIGN KEY (horse_id)
+        REFERENCES public.horses (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT job_proposals_member_horse_rider_id_fkey FOREIGN KEY (member_horse_rider_id)
+        REFERENCES public.members (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT job_proposals_professor_id_fkey FOREIGN KEY (professor_id)
+        REFERENCES public.members (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT job_proposals_rider_id_fkey FOREIGN KEY (rider_id)
+        REFERENCES public.riders (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
+
+ALTER TABLE IF EXISTS public.job_proposals
+    OWNER to grupo04;
+
 
 INSERT INTO job_proposals (institutional_work_proposal, condition, headquarters, days, rider_id, professor_id, member_horse_rider_id, horse_id, assistant_id)
 VALUES ('Hipoterapia', 'Regular', 'CASJ', ARRAY['Lunes', 'Miércoles', 'Viernes'], 1, 1, 1, 1, 1);
--- DB_setup/members.sql
+++ DB_setup/members.sql
@@ -9,5 +9,5 @@ CREATE TABLE members (
     job_id INTEGER NOT NULL REFERENCES jobs(id)
 );
 
-INSERT INTO members (id, email, start_date, end_date, condition, active, profession_id, job_id)
-VALUES (1, 'giuliana_0@gmail.com', '2023-01-01', '2023-12-31', 'Voluntario', TRUE, 1, 1);
+-- INSERT INTO members (id, email, start_date, end_date, condition, active, profession_id, job_id)
+-- VALUES (1, 'giuliana_0@gmail.com', '2023-01-01', '2023-12-31', 'Voluntario', TRUE, 1, 1);
