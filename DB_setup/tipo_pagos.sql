80251c8001e88eb5b8a9a7121bca799a6efe97da
GIULIANA CESTORAME <giulianaacestorame@gmail.com>
2024-10-20 12:28:29 -0300

    fix

--- DB_setup/tipo_pagos.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE public.tipos (
-    id serial PRIMARY KEY,  -- Usamos serial para auto-incrementar
-    tipo varchar(50) NOT NULL
-);
-
-INSERT INTO public.tipos (tipo) VALUES ('Honorarios'), ('Varios'), ('Proveedor');
