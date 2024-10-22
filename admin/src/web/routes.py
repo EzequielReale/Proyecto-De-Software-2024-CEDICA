c414ef39eb82471163020a7d78ab9034adba1dfe
Llamazares Nicolas <llamazares.nico@gmail.com>
2024-10-22 12:43:29 +0000

    Solución de bug en rutas sin '/' al final

--- admin/src/web/routes.py
+++ admin/src/web/routes.py
@@ -5,15 +5,3 @@ def register(app):
     @app.route("/", endpoint="home")
     def home(): 
         return render_template("home.html")
-    
-    @app.route("/jya", endpoint="jya")
-    def jya(): 
-        return render_template("jya/index.html")
-        
-    @app.route("/ecuestre", endpoint="ecuestre")
-    def ecuestre(): 
-        return render_template("ecuestre/index.html")
-        
-    @app.route("/team", endpoint="team")
-    def team(): 
-        return render_template("team/index.html")
