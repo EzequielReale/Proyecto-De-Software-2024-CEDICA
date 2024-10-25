3cac8e9c089d86d7cb28499fd517cab45a7d4d56
Lautaro Josin Saller <ljosinsaller@gmail.com>
2024-10-24 23:42:43 -0300

    Hotfix/cobros

--- admin/src/web/forms/jya_payment_register_form.py
+++ admin/src/web/forms/jya_payment_register_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, DateField, FloatField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, ValidationError, NumberRange, Length
 
 estados_choices = [('', 'Selecciona una opción'),('1', 'En deuda'), ('0', 'Pagado') ]
 
@@ -8,13 +8,22 @@ estados_choices = [('', 'Selecciona una opción'),('1', 'En deuda'), ('0', 'Paga
 class RegisterJyAPaymentForm(FlaskForm):
     """ Clase para validar formulario de registro de nuevo cobro."""
 
+
+    # Validador personalizado para asegurarse de que el valor sea positivo
+    def positive_check(form, field):
+        if field.data is None or field.data <= 0:
+            raise ValidationError('El valor debe ser un número positivo.')
+        
     jya = SelectField('Jinete o Amazona', choices=[] ,validators=[DataRequired()])
     fecha_pago = DateField('Fecha de pago', format='%Y-%m-%d', validators=[DataRequired()])
     medio_de_pago = SelectField('Medio de pago', choices=[], validators=[DataRequired()])
-    monto_pago = FloatField('Monto', validators=[DataRequired()])
+    monto_pago = FloatField('Monto', validators=[DataRequired() , positive_check,
+                                                 NumberRange(min=0, max=9999999, message="El valor debe estar entre 0 y 9999999.")])
     receptor = SelectField('Receptor', choices=[], validators=[DataRequired()])
     estado_de_pago = SelectField('Estado de pago', choices=estados_choices, validators=[DataRequired()])
-    observaciones = StringField('Observaciones', validators=[Optional()])
+    observaciones = StringField('Observaciones', validators=[Optional(),
+                                                             Length(max=99, message="El texto no puede superar los 99 caracteres.")])
     submit = SubmitField('Registrar cobro')
 
 
+    
--- admin/src/web/forms/user_update_form.py
+++ admin/src/web/forms/user_update_form.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectMultipleField
-from wtforms.validators import Optional, Email, EqualTo, DataRequired
+from wtforms.validators import Optional, Email, EqualTo, DataRequired, Length
 
 class UpdateForm(FlaskForm):
     """ Clase para validar formulario de actualización de un usuario."""
 
-    alias = StringField('Alias', validators=[Optional()], default='')
+    alias = StringField('Alias', validators=[Optional(),
+                                             Length(max=50, message="El texto no puede superar los 50 caracteres.")], default='')
 
-    email = StringField('Email', validators=[Optional(), Email(message="Ingrese un correo electrónico válido.")], default='')
+    email = StringField('Email', validators=[Optional(), Email(message="Ingrese un correo electrónico válido."), 
+                                             Length(max=50, message="El texto no puede superar los 50 caracteres.")], default='')
     
     roles_to_delete = SelectMultipleField('Roles para eliminar', choices=[], validators=[Optional()] , default=[])
     
@@ -19,8 +21,10 @@ class UpdateForm(FlaskForm):
 class UpdatePassword(FlaskForm):
     """ Clase para validar formulario de actualización de contraseña de un usuario."""
 
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), 
+                                                    Length(max=50, message="El texto no puede superar los 50 caracteres.")])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), 
+                                                                     Length(max=50, message="El texto no puede superar los 50 caracteres."),
                                                                      EqualTo('password', message="Las contraseñas no coinciden.")])
     submit = SubmitField('Actualizar contraseña')
 
