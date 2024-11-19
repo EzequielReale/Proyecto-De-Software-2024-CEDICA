a71cdcd0bdd328e0bfeeb08cfb8f014342a00b63
Lautaro Josin Saller <ljosinsaller@gmail.com>
2024-11-19 13:03:14 -0300

    Cambios al Home y About del Portal

--- portal/src/components/About.vue
+++ portal/src/components/About.vue
@@ -1,13 +1,72 @@
 <template>
     <section id="about" class="d-flex flex-row justify-content-center align-items-center gap-4">
-        <div class="d-flex flex-column justify-content-center align-items-center gap-4 text-white">
-            <h1 class="">Sobre nosotros</h1>
-            <p>Acá iría el about</p>
+      <div
+        class="d-flex flex-column justify-content-center align-items-center gap-4 text-white"
+        style="max-width: 600px; text-align: justify;"
+      >
+        <h1>Sobre nosotros</h1>
+        <div 
+          class="text-container" 
+          :class="{ 'is-expanded': isExpanded }"
+          ref="textContainer"
+        >
+          {{ aboutText }}
         </div>
+        <button @click="toggleExpand" class="btn btn-link p-0 text-decoration-none">
+          {{ isExpanded ? 'Ver menos' : 'Ver más' }}
+        </button>
+      </div>
     </section>
-</template>
-<style scoped>
-    #about {
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        aboutText:
+          "El Centro de Equitación para Personas con Discapacidad y Carenciadas, CEDICA, es una Asociación Civil sin Fines de Lucro, fundada en 1994 en la ciudad de La Plata, Provincia de Buenos Aires. La misión de CEDICA es la INCLUSIÓN SOCIAL: trabajamos con la finalidad de igualar oportunidades en procura de la reinserción familiar, el logro de la escolaridad, la integración laboral y la mejora integral de la calidad de vida de nuestros alumnos y alumnas. La actividad que realizamos se denomina internacionalmente Rehabilitación Ecuestre y es una terapia integral que apunta a la recuperación de la persona en sus dimensiones biológica, psíquica y social. La Rehabilitación Ecuestre utiliza al caballo de manera terapéutica, aprovechando las características de su naturaleza innata y de su medio ambiente como facilitadores del proceso rehabilitador.",
+        isExpanded: false,
+      };
+    },
+    methods: {
+      toggleExpand() {
+        this.isExpanded = !this.isExpanded;
+  
+        const container = this.$refs.textContainer;
+        if (this.isExpanded) {
+          container.style.height = `${container.scrollHeight}px`;
+        } else {
+          container.style.height = `100px`; 
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .text-container {
+    overflow: hidden;
+    height: 100px; 
+    transition: height 0.5s ease;
+  }
+  
+  .text-container.is-expanded {
+    height: auto;
+  }
+  
+  button {
+    color: #0d6efd;
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    text-decoration: underline;
+  }
+
+
+#about {
         background-image: url(../assets/bg-images/about-bg.png);
         background-size: cover;
         background-position: center;
--- portal/src/components/Home.vue
+++ portal/src/components/Home.vue
@@ -1,45 +1,72 @@
 <template>
     <section id="home" class="d-flex flex-column justify-content-center align-items-center" style="height: 80vh; width: 100%;">
-        <img src="../assets/logos/Logo.png" width="25%" alt="">
-        <div class="mt-3">
-            Acá iría el home
+      <img src="../assets/logos/Logo.png" width="25%" alt="Logo">
+
+      <div class="mt-5 text-center">
+        Centro de Equitación para Personas con Discapacidad y Carenciados
+      </div>
+
+      <div class="mt-4 contact-info d-flex flex-column align-items-center gap-3">
+
+        <!-- Dirección -->
+        <div class="d-flex align-items-center gap-2">
+          
+          <p class="mb-0 text-center">
+            Calle 44 y 115 (1900) La Plata, La Plata, B 1900, Argentina
+          </p>
         </div>
+
+        <!-- Teléfono -->
+        <div class="d-flex align-items-center gap-2">
+          
+          <p class="mb-0 text-center">+54 9 11 6763 0247</p>
+        </div>
+
+      </div>
+
     </section>
-</template>
-<style scoped>
-    #home {
-        margin: 0 !important;
-        padding: 0 !important;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    #home::after {
-        content: '';
-        background-image: url('../assets/bg-images/baner-dec-right.png');
-        background-size: contain;
-        background-repeat: no-repeat;
-        position: absolute;
-        top: 0;
-        right: 0;
-        height: 500px;
-        width: 115px;
-        z-index: -1;
-        opacity: 1;
-    }
-
-    #home::before {
-        content: '';
-        background-image: url('../assets/bg-images/baner-dec-left.png');
-        background-size: contain;
-        background-repeat: no-repeat;
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 500px;
-        width: 150px;
-        z-index: -1;
-        opacity: 1;
-    }
-</style>
+  </template>
+  
+  <style scoped>
+  #home {
+    margin: 0 !important;
+    padding: 0 !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 1rem;
+  }
+  
+  #home::after {
+    content: '';
+    background-image: url('../assets/bg-images/baner-dec-right.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 500px;
+    width: 115px;
+    z-index: -1;
+    opacity: 1;
+  }
+  
+  #home::before {
+    content: '';
+    background-image: url('../assets/bg-images/baner-dec-left.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 500px;
+    width: 150px;
+    z-index: -1;
+    opacity: 1;
+  }
+  
+  .contact-info p {
+    margin: 0;
+  }
+  </style>
